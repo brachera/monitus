0108fcee21403a8f40ad147f7ac9c5a3a59525ed
Aidan Bracher <aidan.bracher@bt.com>
2020-01-09 15:12:30 +0000

    Updated users

--- profiles/lib/facter/users.rb
+++ profiles/lib/facter/users.rb
@@ -7,6 +7,6 @@ require 'etc'
         users << user[0] unless user[2].to_i < 500
     end
     
-    Facter.add(:users) do
+    Facter.add('users') do
       setcode { users.sort.join(',') }
     end
